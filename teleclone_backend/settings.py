fccc834f0fb35bc008460b2c216c7b7427112c2f
dilmurod081 <141741043+dilmurod081@users.noreply.github.com>
2025-08-17 23:07:58 +0500

    Update settings.py

--- teleclone_backend/settings.py
+++ teleclone_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a#b_-0*mmoht^my3lr_7eu*bnm6+d150e&p9o+5wkngw5oyb(s'
+SECRET_KEY = 'django-insecure-yb(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
